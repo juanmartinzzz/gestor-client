484c34c0457cd8725fab7921d455e5341c022bfa
juanmartinzzz <juan.martinzzz@gmail.com>
2019-11-11 00:50:10 -0500

    G-3 Update Order statuses; Add stars (#3)

--- src/components/Orders/orderStatus.js
+++ src/components/Orders/orderStatus.js
@@ -1,23 +1,40 @@
 export const ORDER_STATUS_CONFIRMED = "confirmed";
+export const ORDER_STATUS_REQUESTED = "requested";
 export const ORDER_STATUS_ACCEPTED = "accepted";
 export const ORDER_STATUS_DISPATCHED = "dispatched";
 export const ORDER_STATUS_ARRIVED = "arrived";
+export const ORDER_STATUS_REJECTED = "rejected";
+export const ORDER_STATUS_FAILED = "failed";
+
+let i=0;
 
 export const orderStatusMap = {
+  [ORDER_STATUS_REJECTED]: {
+    value: i++,
+    label: "Rechazado"
+  },
+  [ORDER_STATUS_FAILED]: {
+    value: i++,
+    label: "Fallido"
+  },
+  [ORDER_STATUS_REQUESTED]: {
+    value: i++,
+    label: "Solicitado"
+  },
   [ORDER_STATUS_CONFIRMED]: {
-    value: 3,
+    value: i++,
     label: "Confirmado"
   },
   [ORDER_STATUS_ACCEPTED]: {
-    value: 4,
+    value: i++,
     label: "Aceptado"
   },
   [ORDER_STATUS_DISPATCHED]: {
-    value: 5,
+    value: i++,
     label: "Salió"
   },
   [ORDER_STATUS_ARRIVED]: {
-    value: 6,
+    value: i++,
     label: "Recibido"
-  }
+  },
 };
