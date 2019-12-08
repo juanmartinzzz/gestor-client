e8d4327209b600abee18965e0f3bacfc8cff27ea
Juan M <juan.martinez@districtm.net>
2019-12-08 18:48:33 -0500

    Add margin on every section

--- src/components/components.js
+++ src/components/components.js
@@ -1,5 +1,10 @@
 import styled from "styled-components";
+import SwipeableViews from "react-swipeable-views";
 import { Divider } from "@material-ui/core";
+
+export const SwipeableViewsWithMargin = styled(SwipeableViews)`
+  margin: 24px 0;
+`;
 
 export const CardDivider = styled(Divider)`
   && {
